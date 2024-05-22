9088b643ea5703c4e03f860fb18c182672a77641
Mateus Pestana <matheus.pestana@iesp.uerj.br>
2024-05-22 14:50:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ st.title('Salut')
 with st.sidebar:
   st.header('Salut')
   st.write('Seu aplicativo de dicas de saúde!')
+  st.caption('Criado por Matheus C. Pestana')
+
+st.write('Nosso aplicativo tem como foco oferecer dicas maravilhosas sobre corrida, nutrição e notícias de saúde! Escolha a opção abaixo!') 
+tab_corrida, tab_nutri, tab_news = st.tabs(["Corrida de Rua", "Nutrição", "Notícias"])
